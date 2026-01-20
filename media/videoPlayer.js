25ac476f769d18a3a714cd62566df71c80b52200
magmomentin <magmoment.in@gmail.com>
2026-01-21 02:45:31 +0530

    Update videoPlayer.js

--- media/videoPlayer.js
+++ media/videoPlayer.js
@@ -1,11 +1,21 @@
 export class VideoPlayer {
   constructor(url) {
     this.video = document.createElement("video");
+
+    // 🔑 REQUIRED FOR WEBGL
+    this.video.crossOrigin = "anonymous";
+
     this.video.src = url;
     this.video.loop = true;
     this.video.muted = true;
     this.video.playsInline = true;
   }
-  play() { if (this.video.paused) this.video.play(); }
-  pause() { if (!this.video.paused) this.video.pause(); }
+
+  play() {
+    if (this.video.paused) this.video.play();
+  }
+
+  pause() {
+    if (!this.video.paused) this.video.pause();
+  }
 }
