1aa27392b8c1d98badb81f72032b29b6c54a6abc
magmomentin <magmoment.in@gmail.com>
2026-01-20 23:49:44 +0530

    Add files via upload

--- media/videoPlayer.js
+++ media/videoPlayer.js
@@ -1,1 +1,6 @@
-
+export class VideoPlayer{
+ constructor(url){this.video=document.createElement("video");
+ this.video.src=url; this.video.loop=true; this.video.muted=true; this.video.playsInline=true;}
+ play(){if(this.video.paused)this.video.play();}
+ pause(){if(!this.video.paused)this.video.pause();}
+}
