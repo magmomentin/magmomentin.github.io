946d9d417befc0f3226b0436df0ee82288b7c150
magmomentin <magmoment.in@gmail.com>
2026-01-21 02:02:45 +0530

    Update glRenderer.js

--- renderer/glRenderer.js
+++ renderer/glRenderer.js
@@ -19,17 +19,43 @@ this.p=p;this.b=g.createBuffer();this.t=g.createTexture();
 g.bindTexture(g.TEXTURE_2D,this.t);
 g.texParameteri(g.TEXTURE_2D,g.TEXTURE_MIN_FILTER,g.LINEAR);
 }
-draw(cs){
-const g=this.gl;
-const f=([x,y])=>[(x/g.canvas.width)*2-1,1-(y/g.canvas.height)*2];
-const p=cs.flatMap(f);
-g.bindBuffer(g.ARRAY_BUFFER,this.b);
-g.bufferData(g.ARRAY_BUFFER,new Float32Array(p),g.STATIC_DRAW);
-const l=g.getAttribLocation(this.p,"a");
-g.enableVertexAttribArray(l);
-g.vertexAttribPointer(l,2,g.FLOAT,false,0,0);
-g.bindTexture(g.TEXTURE_2D,this.t);
-g.texImage2D(g.TEXTURE_2D,0,g.RGBA,g.RGBA,g.UNSIGNED_BYTE,this.video);
-g.drawArrays(g.TRIANGLE_STRIP,0,4);
+draw(corners) {
+  const gl = this.gl;
+
+  // ⛔ video not ready yet
+  if (this.video.readyState < 2) return;
+
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  const toClip = ([x, y]) => [
+    (x / gl.canvas.width) * 2 - 1,
+    1 - (y / gl.canvas.height) * 2
+  ];
+
+  const positions = corners.flatMap(toClip);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, this.b);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.DYNAMIC_DRAW);
+
+  const loc = gl.getAttribLocation(this.p, "a");
+  gl.enableVertexAttribArray(loc);
+  gl.vertexAttribPointer(loc, 2, gl.FLOAT, false, 0, 0);
+
+  // 🔑 critical for video textures
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+
+  gl.bindTexture(gl.TEXTURE_2D, this.t);
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGBA,
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    this.video
+  );
+
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 }
+
 }
